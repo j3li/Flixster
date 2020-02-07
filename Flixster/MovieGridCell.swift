e45ee575ce5152d99acad3637cf9a8ac825f67ad
Jing Li <j3li@ucsd.edu>
2020-02-06 17:35:23 -0800

    finished file

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Jing Li on 2/5/20.
+//  Copyright © 2020 lijing3931@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
